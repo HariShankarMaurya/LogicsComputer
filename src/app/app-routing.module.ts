07db8bed9037a5831ef1d14e07256b10b79b78b6
Harishankar <hsmaurya.net@gmail.com>
2022-02-19 18:47:41 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { CourcesComponent } from './Pages/cources/cources.component';
+import { EventsComponent } from './Pages/events/events.component';
+import { IndexComponent } from './Pages/index/index.component';
+import { NotfoundComponent } from './Pages/notfound/notfound.component';
+import { PricingComponent } from './Pages/pricing/pricing.component';
+import { TrainersComponent } from './Pages/trainers/trainers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent} ,
+   {path:'about-us',component:AboutUsComponent},
+   {path:'contact',component:ContactComponent},
+   {path:'cources',component:CourcesComponent},
+   {path:'events',component:EventsComponent},
+   {path:'pricing',component:PricingComponent},
+   {path:'trainer',component:TrainersComponent},
+   {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultHeaderComponent } from './_shared/default-header/default-header.component';
+import { DefaultFooterComponent } from './_shared/default-footer/default-footer.component';
+import { IndexComponent } from './Pages/index/index.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { CourcesComponent } from './Pages/cources/cources.component';
+import { TrainersComponent } from './Pages/trainers/trainers.component';
+import { EventsComponent } from './Pages/events/events.component';
+import { PricingComponent } from './Pages/pricing/pricing.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { NotfoundComponent } from './Pages/notfound/notfound.component';
+import { WhyUsComponent } from './Pages/why-us/why-us.component';
+import { VissionMissionComponent } from './Pages/vission-mission/vission-mission.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultHeaderComponent,
+    DefaultFooterComponent,
+    IndexComponent,
+    AboutUsComponent,
+    CourcesComponent,
+    TrainersComponent,
+    EventsComponent,
+    PricingComponent,
+    ContactComponent,
+    NotfoundComponent,
+    WhyUsComponent,
+    VissionMissionComponent
   ],
   imports: [
     BrowserModule,
